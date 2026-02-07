a8d25ba847614d511e18e8f10e4efbf52c228bcc
Alexey <alexeyli44@gmail.com>
2026-02-08 00:33:35 +0300

    feat: add active state color for primary button and update button variant styles

--- registry/new-york/ui/button.tsx
+++ registry/new-york/ui/button.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "cn-button cursor-pointer inline-flex items-center justify-center whitespace-nowrap transition-[background] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none focus:outline-solid focus:outline-2 group/button select-none",
+  "cn-button cursor-pointer inline-flex items-center justify-center whitespace-nowrap transition-[background] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none focus-visible:outline-solid focus-visible:outline-2 group/button select-none",
   {
     variants: {
       variant: {
